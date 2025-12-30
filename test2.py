207ccd4bdc80dbb99c4abfbd0258ccce5c77c3d6
soradacroi <soradacroi@gmail.com>
2025-12-30 17:01:44 +0530

    Create test2.py

---
+++ test2.py
@@ -0,0 +1,54 @@
+import textmood
+
+# define labels
+LABELS = ['sadness', 'happiness', 'neutral']
+
+# setup stop words and negations
+STOP_WORDS = {"the", "a", "an", "is", "and", "or", "to", "of", "in", "it"}
+NEGATIONS = {"not"}
+textmood.no_words(STOP_WORDS, NEGATIONS)
+
+def main():
+    print(f"training")
+    
+    # load data
+    data = textmood.load_data('tweet_emotions_cleaned.csv',30000 , LABELS, ",")
+
+    # partition data (80% train, 20% test)
+    train_data, test_data = textmood.partition_samples(data, split_p=0.8)
+    print(len(train_data), len(test_data))
+    # train the model
+    textmood.train(train_data, LABELS)
+
+    # test it out
+    test_phrases = {
+        "i am not happy": "sadness",
+        "i am not sad but happy": "happiness",
+        "i am so excited for the test tommorow": "happiness",
+        "hello": "neutral"
+    }
+
+    print("model predictions:\n")
+    for phrase, actual in test_phrases.items():
+        prediction = textmood.predict(phrase, LABELS)
+        print(f"\"{phrase}\"")
+        print(f"predicted: {prediction}, actual: {actual}\n")
+    correct = 0
+    total = len(test_data)
+
+    # evaluation of well acuracy 
+    correct = 0
+    total = len(test_data)
+    print(f"testing on {total} samples\n")
+    for content, actual_label in test_data:
+        prediction = textmood.predict(content, LABELS)
+        if prediction == actual_label:
+            correct += 1
+
+    accuracy = (correct / total) * 100
+    print(f"Accuracy: {accuracy}%")
+    print(f"Correct: {correct}")
+    print(f"Total: {total}")
+
+if __name__ == "__main__":
+    main()
